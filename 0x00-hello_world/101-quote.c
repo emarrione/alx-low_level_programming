dcaa0a53e4ffe7a801d9cc0519f0c2f042eee0a2
emarrione <emarrione@gmail.com>
2022-09-08 15:25:00 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<unistd.h>
 /**
- * main - Prints out the last part of a quote in the standard error.
- * Return: 1 if success.
- */
-int main(void)
+* main - prints the standrad error sandwiched around the  standard output
+* Return: returns 1
+*/
+int main(void)  
 {
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1); 
 }
