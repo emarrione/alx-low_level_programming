c518904169e6a0b02831e26fe4915851591aa12b
emarrione <emarrione@gmail.com>
2022-09-13 11:43:35 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
-#include "main.h"
+#ifndef HEADER_FILE
+#ifndef HEADER_FILE
 /**
 * main - print putchar
 * Return: always 0
@@ -14,5 +15,4 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return (0); 
-}
+#endif
