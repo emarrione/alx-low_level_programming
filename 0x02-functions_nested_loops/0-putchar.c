618f5c216862c1be207f056e8569200ed2fb8657
emarrione <emarrione@gmail.com>
2022-09-13 15:21:20 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
-* main - Print "Holberton" followed by a new line.
-* Description: You are not allowed to include standard libraries.
-* Return: 0
+* main - Entry point
+* description: putchar
+* Return: Always returns 0
 */
 int main(void)
 {
-char _putchar[] = "_putchar";
-int i = 0;
-while _putchar[i] != '\0')
-{
-_putchar(_putchar[i]);
-i++;
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
