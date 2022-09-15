7d8f2fe00598a5882bcab52b2257f801c8af201c
emarrione <emarrione@gmail.com>
2022-09-15 14:52:10 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,10 @@
-#ifndef _MAIN.H_
-#define _MAIN.H_
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+#ifndef MAIN_H
+#define MAIN_H
+/**
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*/
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -12,4 +13,8 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-#endif
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+#endif /* MAIN_H */
