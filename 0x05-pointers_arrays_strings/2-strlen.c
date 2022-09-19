68c93a631dde9b560b716a5f804606496514a4e4
emarrione <emarrione@gmail.com>
2022-09-19 12:50:28 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _strlen - The strlen() accepts an argument of type pointer to char.
+* @s: declaration of *s and paramters for the function _strlen
+* Return: Always 0.
+*/
+int _strlen(char *s)
+{
+int len;
+for (len = 0; s[len] != '\0'; ++len)
+{}
+return (len);
+}
